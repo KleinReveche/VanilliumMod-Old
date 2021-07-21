514719b84119acbe540ade37f3d276402695c700
KleinReveche <personal@kleinreveche.com>
2021-07-21 17:45:27 +0800

    Minor Fixes

--- src/main/java/com/klr2003/anaesia/AnaesiaMod.java
+++ src/main/java/com/klr2003/anaesia/AnaesiaMod.java
@@ -7,8 +7,10 @@ public class AnaesiaMod implements ModInitializer {
 
     public static final String MOD_ID = "anaesia";
     public static final String MOD_NAME = "Anaesia";
-    public static final String VERSION = "0.1.7";
+    public static final String VERSION = "0.1.8";
     public static final String MINECRAFT_ID = "minecraft";
+
+
     /**
      * Runs the mod initializer.
      */
--- src/main/java/com/klr2003/anaesia/utils/ConfigHandler.java
+++ src/main/java/com/klr2003/anaesia/utils/ConfigHandler.java
@@ -31,8 +31,8 @@ public class ConfigHandler {
                 config.put(ConfigList.isZeroTickUnpatchEnabled, "true");
                 config.put(ConfigList.isDebugModeEnabled, "false");
                 config.put(ConfigList.configFileVersion, "1");
-                config.put(ConfigList.isInfinityPatchEnabled, "true");
-                config.put(ConfigList.isDamageEnchantsPatchEnabled, "true");
+                config.put(ConfigList.isInfinityPatchEnabled, "false");
+                config.put(ConfigList.isDamageEnchantsPatchEnabled, "false");
 
                 // save properties to project root folder
                 config.store(output, AnaesiaMod.MOD_NAME + " Mod Configuration");
